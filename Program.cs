a7bcaf587f1e3ae6906ea544dda89c669f63e2d8
Grigorii Skovorodnikov <gskovorod3@gmail.com>
2023-02-12 23:50:52 +0500

    add dz5

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+System.Console.WriteLine("Введите трехзначное число");
+int Number = Convert.ToInt32(Console.ReadLine());
+if (Number >= 1000 || Number < 99)
+{
+    System.Console.WriteLine("Wrong number");
+}
+else
+{
+    Number = Number / 10;
+    int MiddleNumber = Number % 10;
+    System.Console.WriteLine(MiddleNumber);
+}
